183ba1d41890abea8ad92f321d36c0fac7d00955
leo.jaimes <leo777jaimes@gmail.com>
2023-03-07 10:40:58 -0600

    upload

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    // 'prettier/prettier': ['warn', { endOfLine: 'auto' }],
+
+    // 'prettier/prettier': ['error', { endOfLine: 'off' }],
+    'react/react-in-jsx-scope': 0,
+    'react/button-has-type': 0,
+    'import/prefer-default-export': 0,
+    'react/no-array-index-key': 0,
+    'react/require-default-props': 0,
+    'files.eol': 0,
+    'linebreak-style': 0,
+    endOfLine: 0,
+  },
+};
