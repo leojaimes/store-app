20428b4d3d33916be32c7b6aea027a9b423d01e4
leo.jaimes <leo777jaimes@gmail.com>
2023-03-07 12:55:21 -0600

    upload

--- src/form/form.test.tsx
+++ src/form/form.test.tsx
@@ -1,22 +1,36 @@
 import { describe, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { Form } from './form';
 
 describe('when the form is mounted ', () => {
   it('here must be a create product form page', () => {
     ///
     render(<Form />);
-    screen.debug();
+    // screen.debug();
     expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
       'Create Product'
     );
   });
-  it('The form must have the following fields: name, size, type (electronic, furniture, clothing) and a submit button.', () => {
+  it('The form must have the following fields: name, size, type (electronic, furniture, clothing) and a submit button.', async () => {
     render(<Form />);
     const nameTexField = screen.getByLabelText(/name/i);
     expect(nameTexField).toBeInTheDocument();
 
     const sizeTexField = screen.getByLabelText(/size/i);
     expect(sizeTexField).toBeInTheDocument();
+
+    const typeSelect = screen.getByLabelText(/type/i);
+    expect(typeSelect).toBeInTheDocument();
+
+    expect(screen.getByText(/electronic/i)).toBeInTheDocument();
+    expect(screen.getByText(/furniture/i)).toBeInTheDocument();
+    expect(screen.getByText(/clothing/i)).toBeInTheDocument();
+    screen.debug();
+    // const typeButtonSelect = screen.getByRole('button', { name: /type/i });
+    // expect(typeButtonSelect).toBeInTheDocument();
+    // fireEvent.click(typeButtonSelect);
+
+    // expect(await screen.findByText(/electronic/i)).toBeInTheDocument();
+    // screen.debug();
   });
 });
--- src/form/form.tsx
+++ src/form/form.tsx
@@ -1,4 +1,13 @@
-import { CssBaseline, Typography, TextField } from '@mui/material';
+import {
+  CssBaseline,
+  Typography,
+  TextField,
+  Select,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  NativeSelect,
+} from '@mui/material';
 import Container from '@mui/material/Container';
 
 export function Form() {
@@ -6,8 +15,25 @@ export function Form() {
     <Container maxWidth="xs">
       <CssBaseline />
       <Typography component="h1">Create Product</Typography>
-      <TextField label="name" />
-      <TextField label="size" />
+      <TextField label="name" id="name" name="name" />
+      <TextField label="size" id="size" name="size" />
+
+      <FormControl fullWidth>
+        <InputLabel variant="standard" htmlFor="type">
+          Type
+        </InputLabel>
+        <NativeSelect
+          inputProps={{
+            name: 'type',
+            id: 'type',
+          }}
+        >
+          <option aria-label="None" value="" />
+          <option value="electronic">Electronic</option>
+          <option value="furniture">Furniture</option>
+          <option value="clothing">Clothing</option>
+        </NativeSelect>
+      </FormControl>
     </Container>
   );
 }
