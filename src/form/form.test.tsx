d606cddb9acde9e073adc19d3f065763e0082879
leo.jaimes <leo777jaimes@gmail.com>
2023-03-07 11:19:20 -0600

    upload

---
+++ src/form/form.test.tsx
@@ -0,0 +1,10 @@
+import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import { Form } from './form';
+
+describe('when the form is mounted ', () => {
+  it('here must be a create product form page', () => {
+    ///
+    render(<Form />);
+  });
+});
