4b34bf0f8cdda86771667d7ae5d4f3a615b84b3c
leo.jaimes <leo777jaimes@gmail.com>
2023-04-10 13:59:51 -0600

    upload

--- src/components/error-boundary/ErrorBoundary.test.tsx
+++ src/components/error-boundary/ErrorBoundary.test.tsx
@@ -5,19 +5,45 @@ import {
   waitFor,
   within,
 } from '@testing-library/react';
+import { vitest } from 'vitest';
 import { ErrorBoundary } from './ErrorBoundary';
 
+vitest.spyOn(console, 'error');
+
 describe('when the component works without errors', () => {
-  render(<ErrorBoundary />);
-  it('must render the component content', () => {});
+  it('must render the component content', () => {
+    render(
+      <ErrorBoundary>
+        <h1>Test Pass </h1>
+      </ErrorBoundary>
+    );
+    expect(screen.getByText(/test pass/i)).toBeInTheDocument();
+  });
 });
 
 describe('when the component throws errors', () => {
-  render(<ErrorBoundary />);
-  it('must render the message "There is an unexpeded error" and a reload button', () => {});
+  it('must render the message "There is an unexpeded error" and a reload button', () => {
+    function ThrowError() {
+      throw new Error('ups');
+      return (
+        <>
+          <div />
+          <div />
+        </>
+      );
+    }
+    render(
+      <ErrorBoundary>
+        <ThrowError />
+      </ErrorBoundary>
+    );
+
+    expect(
+      screen.getByText(/There is an unexpected error/)
+    ).toBeInTheDocument();
+  });
 });
 
 describe('when the user clicks on reload button', () => {
-  render(<ErrorBoundary />);
   it('must reload the app', () => {});
 });
--- src/components/error-boundary/ErrorBoundary.tsx
+++ src/components/error-boundary/ErrorBoundary.tsx
@@ -1,5 +1,49 @@
-import React from 'react';
+import { Button, Typography } from '@mui/material';
+import React, { ReactNode } from 'react';
 
-export function ErrorBoundary() {
-  return <div>ErrorBoundary</div>;
+interface Props {
+  children?: ReactNode;
+}
+interface MyState {
+  hasError: boolean;
+}
+
+export class ErrorBoundary extends React.Component<Props, MyState> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  handleReloadClick() {
+    // this.setState({ hasError: false });
+    window.location.reload();
+  }
+
+  render() {
+    const { children } = this.props;
+    const { hasError } = this.state;
+
+    if (hasError) {
+      return (
+        <>
+          <Typography variant="h4">There is an unexpected error</Typography>
+          <Button
+            type="button"
+            onClick={this.handleReloadClick}
+            variant="contained"
+            color="primary"
+          >
+            Reload
+          </Button>
+        </>
+      );
+    }
+
+    return children;
+  }
 }
