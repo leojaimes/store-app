6b44144b0a6622cad48e11e1d7d2e4506c905b6e
leo.jaimes <leo777jaimes@gmail.com>
2023-04-11 14:02:03 -0600

    upload

--- src/auth/components/login-page/LoginPage.test.tsx
+++ src/auth/components/login-page/LoginPage.test.tsx
@@ -1,7 +1,12 @@
 import { render, screen } from '@testing-library/react';
+import { LoginPage } from './LoginPage';
 
-describe('App', () => {
-  it('Renders hello world', () => {
+describe('when loging page is mounted', () => {
+  it('must display login title', () => {
     ///
+    render(<LoginPage />);
+    expect(
+      screen.getByRole('heading', { name: /login page/i })
+    ).toBeInTheDocument();
   });
 });
--- src/auth/components/login-page/LoginPage.tsx
+++ src/auth/components/login-page/LoginPage.tsx
@@ -1,5 +1,6 @@
+import { Typography } from '@mui/material';
 import React from 'react';
 
 export function LoginPage() {
-  return <div>LoginPage</div>;
+  return <Typography variant="h1">Login Page</Typography>;
 }
