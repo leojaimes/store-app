42dca24eca5555e6534781ea72e47beb5bda621b
leo.jaimes <leo777jaimes@gmail.com>
2023-04-26 10:28:14 -0600

    up0load

--- src/pages/login-page/LoginPage.test.tsx
+++ src/pages/login-page/LoginPage.test.tsx
@@ -1,11 +1,26 @@
 import { screen, render } from '@testing-library/react';
 import { LoginPage } from './LoginPage';
 
+const getEmailTextField = () => screen.getByRole('textbox', { name: /email/i });
+const getPasswordTextField = () =>
+  screen.getByRole('textbox', { name: /password/i });
+
 describe('when login page is mounted', () => {
   it('should be show login title', () => {
     render(<LoginPage />);
     expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
       /login page/i
     );
+  });
+});
+
+describe('when login page is mounted', () => {
+  it('should be show email text field', () => {
+    render(<LoginPage />);
+
+    const emailTextField = getEmailTextField();
+    const passwordTextField = getPasswordTextField();
+    expect(emailTextField).toBeInTheDocument();
+    expect(passwordTextField).toBeInTheDocument();
   });
 });
--- src/pages/login-page/LoginPage.tsx
+++ src/pages/login-page/LoginPage.tsx
@@ -1,10 +1,14 @@
-import { Typography } from '@mui/material';
+import { TextField, Typography } from '@mui/material';
 import React from 'react';
 
 export function LoginPage() {
   return (
-    <Typography component="h1" variant="h5">
-      Login Page
-    </Typography>
+    <>
+      <Typography component="h1" variant="h5">
+        Login Page
+      </Typography>
+      <TextField id="email" name="email" label="email" />
+      <TextField id="password" name="password" label="password" />
+    </>
   );
 }
