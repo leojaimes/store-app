18ea72f1cd2df04ee5bd295fb5d58e5613555ea7
leo.jaimes <leo777jaimes@gmail.com>
2023-04-26 11:13:00 -0600

    upload

--- src/pages/login-page/LoginPage.test.tsx
+++ src/pages/login-page/LoginPage.test.tsx
@@ -1,9 +1,13 @@
-import { screen, render } from '@testing-library/react';
+import { screen, render, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import { LoginPage } from './LoginPage';
 
-const getEmailTextField = () => screen.getByRole('textbox', { name: /email/i });
-const getPasswordTextField = () =>
+const EmailTextField = () => screen.getByRole('textbox', { name: /email/i });
+const PasswordTextField = () =>
   screen.getByRole('textbox', { name: /password/i });
+
+const SubmitButton = () => screen.getByRole('button', { name: /submit/i });
 
 describe('when login page is mounted', () => {
   it('should be show login title', () => {
@@ -18,9 +22,24 @@ describe('when login page is mounted', () => {
   it('should be show email text field', () => {
     render(<LoginPage />);
 
-    const emailTextField = getEmailTextField();
-    const passwordTextField = getPasswordTextField();
+    const emailTextField = EmailTextField();
+    const passwordTextField = PasswordTextField();
+    const submitButton = SubmitButton();
+
     expect(emailTextField).toBeInTheDocument();
     expect(passwordTextField).toBeInTheDocument();
+    expect(submitButton).toBeInTheDocument();
+  });
+});
+describe('when user click submit', () => {
+  it('should show this field is required', async () => {
+    render(<LoginPage />);
+
+    expect(screen.queryByText(/email is required/)).not.toBeInTheDocument();
+    expect(screen.queryByText(/password is required/)).not.toBeInTheDocument();
+
+    await userEvent.click(SubmitButton());
+    expect(screen.getByText(/email is required/)).toBeInTheDocument();
+    expect(screen.getByText(/password is required/)).toBeInTheDocument();
   });
 });
--- src/pages/login-page/LoginPage.tsx
+++ src/pages/login-page/LoginPage.tsx
@@ -1,14 +1,50 @@
-import { TextField, Typography } from '@mui/material';
-import React from 'react';
+import { Button, TextField, Typography } from '@mui/material';
+import React, { useState } from 'react';
 
 export function LoginPage() {
+  const [email, setEmail] = useState<string>('');
+
+  const [password, setPassword] = useState<string>('');
+
+  const [emailHelperText, setEmailHelperText] = useState<string>('');
+  const [passwordHelperText, setPasswordHelperText] = useState<string>('');
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!email) {
+      setEmailHelperText('email is required');
+    }
+    if (!password) {
+      setPasswordHelperText('password is required');
+    }
+  };
   return (
-    <>
+    <form
+      onSubmit={handleSubmit}
+      id="loginForm"
+      name="loginForm"
+      className="loginForm"
+    >
       <Typography component="h1" variant="h5">
         Login Page
       </Typography>
-      <TextField id="email" name="email" label="email" />
-      <TextField id="password" name="password" label="password" />
-    </>
+      <TextField
+        id="email"
+        name="email"
+        label="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        helperText={emailHelperText}
+      />
+      <TextField
+        id="password"
+        name="password"
+        label="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        helperText={passwordHelperText}
+      />
+      <Button type="submit">Submit</Button>
+    </form>
   );
 }
