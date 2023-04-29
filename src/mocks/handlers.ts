3d1db7fcda40eb1389479124739b026b01712a18
leo.jaimes <leo777jaimes@gmail.com>
2023-04-29 12:24:02 -0600

    upload

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,69 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { ResponseComposition, RestContext, RestRequest, rest } from 'msw';
+
+export interface SignInPostRequestBody {
+  email: string;
+  password: string;
+}
+
+// Describe the shape of the mocked response body.
+interface SignInPostResponseBody {
+  message: string;
+  email: string;
+  password: string;
+}
+
+// Describe the shape of the "req.params".
+interface SignInPostRequestParams {
+  param1: string;
+}
+
+const loginRequest = async (
+  req: RestRequest<SignInPostRequestBody>,
+  res: ResponseComposition<SignInPostResponseBody>,
+  ctx: RestContext
+) => {
+  sessionStorage.setItem('is-authenticated', 'true');
+
+  const { email, password } = req.body;
+  return res(
+    ctx.status(200),
+    ctx.json({
+      message: 'ok',
+      email,
+      password,
+    })
+  );
+};
+
+export const handlers = [
+  rest.post<SignInPostRequestBody, SignInPostResponseBody>(
+    '/login',
+    async (req, res, ctx) => {
+      sessionStorage.setItem('is-authenticated', 'true');
+
+      const { email, password } = req.body;
+      const role = 'user';
+      const name = 'user';
+
+      return res(
+        ctx.status(200),
+        ctx.json({
+          message: 'ok',
+          email,
+          password,
+          user: {
+            role,
+            email,
+            name,
+          },
+        })
+      );
+    }
+  ),
+];
+
+export default { handlers };
+
+// Revisar este codigo en este link
+// https://dev.to/kettanaito/type-safe-api-mocking-with-mock-service-worker-and-typescript-21bf//
