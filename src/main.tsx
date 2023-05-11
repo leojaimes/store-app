c0212c2af09156a3d353c2a5ba99e960493180b1
leo.jaimes <leo777jaimes@gmail.com>
2023-05-11 12:58:11 -0600

    upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { CssBaseline } from '@mui/material';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 import './index.css';
@@ -15,6 +16,7 @@ if (process.env.NODE_ENV === 'development') {
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <CssBaseline />
       <App />
     </QueryClientProvider>
   </React.StrictMode>
