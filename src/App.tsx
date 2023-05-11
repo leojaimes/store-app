5f9d6ebd08ea43c2809b298749746cd14e2ba81c
leo.jaimes <leo777jaimes@gmail.com>
2023-05-11 12:27:21 -0600

    upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
 import { Form } from './form/form';
 import { LoginPage } from './pages/login-page/LoginPage';
 
+const queryClient = new QueryClient();
 function App() {
-  return <LoginPage />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <LoginPage />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
